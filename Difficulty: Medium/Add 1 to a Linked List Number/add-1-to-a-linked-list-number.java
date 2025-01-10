feacc01b9d3eb276266bb70b34141459a9e8cde8
pratikk7007 <113412711+pratikk7007@users.noreply.github.com>
2025-01-10 07:00:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.java
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+class GfG {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data);
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+            Node head = new Node(Integer.parseInt(str[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(str[i]));
+                tail = tail.next;
+            }
+            Solution obj = new Solution();
+            head = obj.addOne(head);
+            printList(head);
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public Node addOne(Node head) {
+        // Reverse the linked list
+        Node prev = null;
+        Node curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev; // Update the head to the reversed list
+
+        // Add one to the reversed list
+        Node temp = head;
+        int carry = 1;
+        while (temp != null) {
+            temp.data += carry;
+            if (temp.data >= 10) {
+                temp.data -= 10;
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+            if (carry != 0 && temp.next == null) {
+                temp.next = new Node(1);
+                carry = 0;
+            }
+            temp = temp.next;
+        }
+
+        // Reverse the list again to restore the original order
+        prev = null;
+        curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev; // Update the head to the final reversed list
+
+        return head;
+    }
+}
