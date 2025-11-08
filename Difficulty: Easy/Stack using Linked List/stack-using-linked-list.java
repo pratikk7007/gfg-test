0568f60d896fe7b04b37c29992d07a65aed45e6d
pratikk7007 <113412711+pratikk7007@users.noreply.github.com>
2025-11-08 10:39:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Stack using Linked List/stack-using-linked-list.java
+++ Difficulty: Easy/Stack using Linked List/stack-using-linked-list.java
@@ -1,82 +1,55 @@
-//{ Driver Code Starts
-import java.util.*;
-
-class StackNode {
+// Node class
+/* class Node {
     int data;
-    StackNode next;
+    Node next;
 
-    StackNode(int a) {
-        data = a;
+    Node(int new_data) {
+        data = new_data;
         next = null;
     }
-}
+} */
 
-class GfG {
+// Stack class
+class myStack {
+    Node temp;
+    int size;
 
-    public static void main(String args[]) {
-        Scanner scanner = new Scanner(System.in);
-        int T = scanner.nextInt();
-        scanner.nextLine(); // Flush the newline character left by nextInt
-        while (T-- > 0) {
-            MyStack sq = new MyStack();
-            String line = scanner.nextLine();
-            Scanner ss = new Scanner(line);
-            List<Integer> nums = new ArrayList<>();
-            while (ss.hasNextInt()) {
-                int num = ss.nextInt();
-                nums.add(num);
-            }
-            int n = nums.size();
-            int i = 0;
-            while (i < n) {
-                int QueryType = nums.get(i++);
-                if (QueryType == 1) {
-                    int a = nums.get(i++);
-                    sq.push(a);
-                } else if (QueryType == 2) {
-                    System.out.print(sq.pop() + " ");
-                }
-            }
-            System.out.println();
+    public myStack() {
+        // Initialize your data members
+        temp = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        // check if the stack is empty
+        return temp == null;
+    }
+
+    public void push(int x) {
+        // Adds an element x at the rear of the stack.
+        Node newNode = new Node(x);
+        newNode.next = temp;
+        temp = newNode;
+        size++;
+    }
+
+    public void pop() {
+        // Removes the front element of the stack.
+        if (temp != null) {
+            temp = temp.next;
+            size--;
+        }
+    }
+
+    public int peek() {
+        // Returns the front element of the stack.
         
-System.out.println("~");
-}
-        scanner.close();
-    }
-}
-
-// } Driver Code Ends
-
-
-class MyStack {
-    // class StackNode {
-    //     int data;
-    //     StackNode next;
-    //     StackNode(int a) {
-    //         data = a;
-    //         next = null;
-    //     }
-    // }
-    StackNode top;
-
-    // Function to push an integer into the stack.
-    void push(int a) {
-        StackNode node = new StackNode(a);
-        if (top == null) {
-            top = node;
-        } else {
-            node.next = top;
-            top = node;
-        }
+        if (temp == null) return -1;
+        return temp.data;
     }
 
-    // Function to remove an item from top of the stack.
-    int pop() {
-        if (top == null) {
-            return -1; 
-        }
-        int result = top.data;
-        top = top.next; 
-        return result;
+    public int size() {
+        // Returns the current size of the stack.
+        return size;
     }
 }
